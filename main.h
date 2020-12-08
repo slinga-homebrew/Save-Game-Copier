333645be90683275257fbca5d5f097922625d4b0
slinga-homebrew <58894244+slinga-homebrew@users.noreply.github.com>
2020-12-07 22:44:06 -0500

    Bumped version number

--- main.h
+++ main.h
@@ -30,7 +30,7 @@
 #pragma once
 
 // program version, keep this length to avoid having to resize strings
-#define VERSION "3.2.1"
+#define VERSION "3.2.2"
 
 // program states
 #define STATE_UNINITIALIZED      0
