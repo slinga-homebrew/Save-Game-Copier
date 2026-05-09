ac886688cfb329516482a8eabec78e6b3e9e47ce
slinga-homebrew <58894244+slinga-homebrew@users.noreply.github.com>
2026-05-09 00:11:36 -0400

    Refactor VCD Card support to use Jo Engine

--- backends/vcd_card.c
+++ backends/vcd_card.c
@@ -1,15 +1,10 @@
+#include <jo/vcd_card.h>
 #include "vcd_card.h"
 
-#define BIOS_IS_VCD_CARD_PRESENT (0x06000274) // contains a pointer to bios_is_mpeg_card_present routine
-#define BIOS_GET_VCD_CARD_ROM (0x06000298) // contains a pointer to bios_is_mpeg_card_present routine
-
-typedef int (*bios_is_vcd_card_present_fn)(int filtno);
-typedef int (*bios_get_vcd_card_rom_fn)(unsigned int index, unsigned int size, void* dest);
 
 // Check if the VCD card is present
 bool vcdIsBackupDeviceAvailable(int backupDevice)
 {
-    bios_is_vcd_card_present_fn bios_is_vcd_card_present = NULL;
     int result = 0;
 
     if(backupDevice != VCDCardBackup)
@@ -17,17 +12,10 @@ bool vcdIsBackupDeviceAvailable(int backupDevice)
         return false;
     }
 
-    bios_is_vcd_card_present = (bios_is_vcd_card_present_fn)(*(unsigned int*)BIOS_IS_VCD_CARD_PRESENT);
-    if(!bios_is_vcd_card_present)
-    {
-        sgc_core_error("VCD: Failed to lookup bios func1\n");
-        return false;
-    }
-
-    result = bios_is_vcd_card_present(0);
+    result = jo_vcd_card_is_present();
     if(result != 0)
     {
-        sgc_core_error("VCD not present %x", result);
+        //sgc_core_error("VCD not present %x", result);
         return false;
     }
 
@@ -67,8 +55,7 @@ int vcdListSaveFiles(int backupDevice, PSAVES saves, unsigned int numSaves)
 int vcdReadSaveFile(int backupDevice, char* filename, unsigned char* outBuffer, unsigned int outSize)
 {
     UNUSED_ARG(filename);
-
-    bios_get_vcd_card_rom_fn bios_get_vcd_card_rom = NULL;
+    
     int result = 0;
 
     if(backupDevice != VCDCardBackup)
@@ -88,15 +75,8 @@ int vcdReadSaveFile(int backupDevice, char* filename, unsigned char* outBuffer, 
         return -2;
     }
 
-    bios_get_vcd_card_rom = (bios_get_vcd_card_rom_fn)(*(unsigned int*)BIOS_GET_VCD_CARD_ROM);
-    if(!bios_get_vcd_card_rom)
-    {
-        sgc_core_error("VCD: Failed to lookup bios func2\n");
-        return -3;
-    }
-
     // TODO: what are these magic numbers
-    result = bios_get_vcd_card_rom(246, 128, outBuffer);
+    result = jo_vcd_card_get_vcd_card_rom(246, 128, outBuffer, outSize);
     if(result < 0)
     {
         sgc_core_error("VCD: Read 1 error %x\n", result);
@@ -104,7 +84,7 @@ int vcdReadSaveFile(int backupDevice, char* filename, unsigned char* outBuffer, 
     }
     sgc_core_error("VCD: Read 1 Success %x\n", result);
 
-    result = bios_get_vcd_card_rom(246 + 128, 128, outBuffer + 0x40000);
+    result = jo_vcd_card_get_vcd_card_rom(246 + 128, 128, outBuffer + VCD_CARD_FIRMWARE_SIZE/2, outSize);
     if(result < 0)
     {
         sgc_core_error("VCD: Read 2 error %x\n", result);
